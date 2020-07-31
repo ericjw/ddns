b8230058288568f022a096caf24c25dff15593a8
Eric Walters <ericjw@vt.edu>
2020-07-31 14:41:13 -0400

    dynamically get IP using ipify

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-fqdn = "ewalters.xyz"
-api_key = "" 
+fqdn = "example.com"
+api_key = "abc123" 
 rrset_name = "@" # name of the record to update
 rrset_type = "A" # type of record to update
+ttl = 10800 # the time in seconds that DNS resolvers should cache this record
--- dynamic_dns_gandi.py
+++ dynamic_dns_gandi.py
@@ -2,10 +2,13 @@ import requests
 import datetime
 from config import *
 
+ip = requests.get('https://api.ipify.org').text # get public IP
 headers = { 'Content-Type': 'application/json', 'Authorization': "Apikey {}".format(api_key)}
 url = "https://api.gandi.net/v5/livedns/domains/{}/records/{}/{}".format(fqdn, rrset_name, rrset_type)
-body = {"rrset_ttl": 10800, "rrset_values": ["24.255.70.235"]}
+body = {"rrset_ttl": ttl, "rrset_values": [ip]}
+
 r = requests.put(url, headers=headers, json=body)
 
 
+# logging
 print("{}    ----    {}".format(datetime.datetime.now().strftime("%c"), r.text))
